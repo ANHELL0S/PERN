4466c98168c64a9ae63c0b42f216216c2ef38756
Angelo <aangelogarcia2021@gmail.com>
2024-11-26 16:32:19 -0500

    fix for new animations reponsive

--- client/src/sections/Sidebar/components/SidebarItem.jsx
+++ client/src/sections/Sidebar/components/SidebarItem.jsx
@@ -1,15 +1,13 @@
 import { useLocation } from 'react-router-dom'
 
-const SidebarItem = ({ path, icon: Icon, label, isCollapsed, onClick }) => {
+const SidebarItem = ({ path, icon: Icon, label, isCollapsed }) => {
 	const location = useLocation()
 
 	const isActive =
 		location.pathname === path ? 'bg-neutral-200/50 text-neutral-600' : 'hover:bg-neutral-200/50 hover:text-neutral-600'
 
 	return (
-		<li
-			className={`flex items-center gap-2 p-1 rounded-md w-full cursor-pointer transition-all duration-200 ease-linear ${isActive}`}
-			onClick={onClick}>
+		<li className={`flex items-center gap-2 p-1.5 rounded-md w-full cursor-pointer ${isActive}`}>
 			<Icon size={24} />
 			{!isCollapsed && <span>{label}</span>}
 		</li>
